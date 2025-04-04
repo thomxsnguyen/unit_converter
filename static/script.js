a907344e9751bdc51eb342f8d72df3c135b23bf7
Thomas <thomasn8255@gmail.com>
2025-04-03 20:16:26 -0700

    added another  page  for reset, changed html formatting

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ document
   .getElementById("submit_button")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const result = document.getElementById("result");
+    const result = document.getElementById("converted-result");
     const length = document.getElementById("length").value;
     const from = document.getElementById("from").value;
     const to = document.getElementById("to").value;
@@ -18,8 +18,15 @@ document
       .then((data) => {
         console.log("Server Response:", data);
         result.innerText = data.converted;
+        document.querySelector(".form1").style.display = "none";
+        document.getElementById("second-box").style.display = "block";
       })
       .catch((err) => {
         console.error("Fetch failed:", err);
       });
   });
+
+function resetForm() {
+  document.querySelector(".form1").style.display = "block";
+  document.getElementById("second-box").style.display = "none";
+}
